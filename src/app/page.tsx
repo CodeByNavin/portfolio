4aafa94c5a056f12a623f8d442acd160c8a9e66e
CodeByNavin <bedinavinyt@gmail.com>
2025-06-28 23:05:44 -0400

    Fixed page divs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,9 +27,13 @@ export default function Home() {
 	return (
 		<>
 			<div className='flex flex-col relative min-h-screen bg-neutral-800 pb-10 overflow-y-auto overflow-x-hidden'>
-				<section className='flex flex-1 h-screen w-full text-white z-10 max-xl:flex-col items-center justify-center'>
+				<section className="flex flex-1 h-screen w-full text-white z-10
+					flex-row items-center justify-center pl-32 max-lg:pl-0 pr-8 max-lg:pr-0
+					max-xl:flex-col max-xl:items-center max-xl:justify-center max-xl:px-4
+					max-sm:px-2
+				">
 					{/* Left: Icon, Name, Role */}
-					<div className='flex flex-col items-center justify-center w-1/3 max-xl:w-full gap-4 p-8'>
+					<div className='flex flex-col items-center justify-center w-1/3 max-xl:w-full max-w-md gap-4 p-4 max-sm:p-2'>
 						<Image
 							src='/Navin.png'
 							alt='Navin'
@@ -42,9 +46,9 @@ export default function Home() {
 					</div>
 
 					{/* Right: Info, Frameworks, Languages */}
-					<div className='flex flex-col w-2.8/3 max-xl:w-full gap-8 p-8'>
+					<div className='flex flex-col w-2/3 max-xl:w-full gap-8 p-8 max-lg:p-6 max-sm:p-4 items-center max-xl:items-center'>
 						{/* Info text inline with name/role */}
-						<div className='relative flex flex-col gap-2 h-fit p-8 max-lg:gap-4 max-lg:px-24 max-lg:text-nowrap max-xl:max-w-xl 2xl:max-w-xl mt-10'>
+						<div className='relative flex flex-col gap-2 h-fit p-8 max-lg:p-6 max-lg:gap-4 max-xl:max-w-xl 2xl:max-w-xl mt-10 max-xl:mt-0 max-sm:p-4'>
 							<div className='absolute w-1 h-12 top-0 left-0 bg-red-500' />
 							<div className='absolute w-1 h-12 -top-6 left-[1.25rem] translate-x-1/2 bg-red-500 rotate-90' />
 							<p className='text-lg max-w-2xl text-wrap'>
@@ -54,9 +58,9 @@ export default function Home() {
 							<div className='absolute w-1 h-12 -bottom-6 right-[1.50rem] translate-x-1/2 bg-red-500 rotate-90' />
 						</div>
 						{/* Frameworks */}
-						<div className='flex flex-col items-start gap-2 w-full'>
+						<div className='relative flex flex-col items-center gap-2 w-full max-w-2xl'>
 							<h1 className='text-2xl font-poppins'>Frameworks</h1>
-							<div className='flex flex-wrap items-center gap-2 w-full'>
+							<div className='flex flex-wrap items-center justify-center gap-2 w-full'>
 								{data.frameworks.map((framework) => (
 									<div
 										key={framework.name}
@@ -67,10 +71,11 @@ export default function Home() {
 								))}
 							</div>
 						</div>
+
 						{/* Languages */}
-						<div className='flex flex-col items-start gap-2 w-full'>
+						<div className='relative flex flex-col items-center gap-2 w-full max-w-2xl'>
 							<h1 className='text-2xl font-poppins'>Languages</h1>
-							<div className='flex flex-wrap items-center gap-2'>
+							<div className='flex flex-wrap items-center justify-center gap-2'>
 								{data.languages.map((language) => (
 									<div
 										key={language.name}
@@ -85,10 +90,10 @@ export default function Home() {
 				</section>
 
 				{projects.length > 0 && (
-					<section>
+					<section className="pl-32 max-lg:pl-0 pr-8 max-lg:pr-0 max-lg:px-4 max-sm:px-2">
 						{/* Projects */}
 						<h1 className='text-2xl font-poppins text-center text-white mt-8'>Projects</h1>
-						<div className='flex flex-wrap justify-center gap-4 p-8'>
+						<div className='flex flex-wrap justify-center gap-4 p-8 max-sm:p-4'>
 							{projects?.map((project) => (
 								<ProjectCard
 									key={project.title}
